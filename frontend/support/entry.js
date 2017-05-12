d98b4450fc946e5c18c89d45bd57b1888cc45af2
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-12 13:38:46 +0900

    Updating files to new version of pux.

---
+++ frontend/support/entry.js
@@ -0,0 +1,21 @@
+const ClientEntry = require('../src/Client.purs');
+
+const initialState = ClientEntry.readState(window.__puxInitialState);
+
+// If hot-reloading, hook into each state change and re-render using the last
+// state.
+if (module.hot) {
+  let initState = window.__puxLastState || initialState
+  let app = ClientEntry.main(window.location.pathname)(initState)()
+
+  // Hook for pux devtools
+  window.__puxApp = app;
+
+  app.state.subscribe((state) => {
+   window.__puxLastState = state;
+  });
+
+  module.hot.accept();
+} else {
+  ClientEntry.main(window.location.pathname)(initialState)()
+}
