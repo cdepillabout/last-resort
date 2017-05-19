8990efa9476d6a260f8865944aee9345e4515072
(cdep)illabout <cdep.illabout@gmail.com>
2017-05-19 15:26:04 +0900

    Add Debug.Trace functions to Prelude.

--- frontend/src/LastResort/Prelude.purs
+++ frontend/src/LastResort/Prelude.purs
@@ -1,10 +1,15 @@
 
 module LastResort.Prelude
-  ( module Prelude
+  ( module Debug.Trace
+  , module Prelude
   , undefined
   ) where
 
 import Prelude
+
+import Debug.Trace
+  (class DebugWarning, spy, trace, traceA, traceAny, traceAnyA, traceAnyM,
+   traceShow, traceShowA, traceShowM)
 import Unsafe.Coerce (unsafeCoerce)
 
 undefined :: forall a. a
